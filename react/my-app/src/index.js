2a26f6a262d2dfc32805a4f971e067d910e44519
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-02-01 15:05:47 +0900

    chapter06 까지 예습습

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import Clock from "./chapter04/Clock";
-import You from "./you/You";
+// import Clock from "./chapter04/Clock";
+// import You from "./you/You";
+// import CommentList from "./chapter05/CommentList";
+import NotificationList from "./chapter06/NotificationList";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 setInterval(() => {
   root.render(
     <React.StrictMode>
-      <You />
+      <NotificationList />
     </React.StrictMode>
   );
 }, 1000);
