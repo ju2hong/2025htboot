50b10467448f080f0c6263e3e2c6c806c8ae5be5
ju2hong <juihong730@gmail.com>
2025-02-12 14:31:37 +0900

    14-context 15-bar 실습

---
+++ react/my-app/src/App14.js
@@ -0,0 +1,9 @@
+import ThemeContextProvider from "./14-context/ThemeContextProvider";
+function App() {
+  return (
+    <div>
+      <ThemeContextProvider inittheme="dark" />
+    </div>
+  );
+}
+export default App;
