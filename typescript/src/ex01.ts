98e55e3fc9dcb30ae58853f9b6c7004b1eff6679
ju2hong <juihong730@gmail.com>
2025-02-24 10:53:16 +0900

    typescript

---
+++ typescript/src/ex01.ts
@@ -0,0 +1,22 @@
+// 기본타입 (primitive type) : 하나의 값만 가지는 타입
+
+// 명시적 타입선언 vs 암묵적 타입선언
+// let myNum1: number = 123
+// let myNum2 = 123
+
+// number
+let num1: number = 123
+// string
+let str1: string = 'hello'
+// boolean
+let bool1: boolean = true
+// null
+let null1: null = null
+// undefined
+let unde1: undefined = undefined
+// 리터럴 타입
+let lit1: 10 = 10
+
+// "strictNullChecks": true
+// null값을 변수에 할당하기 못하도록 한다.
+let numA: number = null
---
+++ typescript/src/ex03.ts
@@ -0,0 +1,34 @@
+// 객체타입
+let user1 = {
+  id: 1,
+  name: '홍길동',
+}
+
+// 'objext'형식에 'id'속성은 없습니다.
+user1.id
+
+// 객체 리터럴 타입
+let user2: {
+  id: number
+  name: string
+} = {
+  id: 1,
+  name: '홍길동',
+}
+user2.id
+
+let user3: {
+  // 선택적 프로퍼티를 통햏 속성 생략 가능
+  id?: number
+  name: string
+} = {
+  name: '홍길동',
+}
+
+let config: {
+  // 읽기전용 속성으로 지정
+  readonly apiKey: string
+} = {
+  apiKey: 'MY API KEY',
+}
+config.apiKey
