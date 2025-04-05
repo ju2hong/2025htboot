f73dadf601e2363e4e6c0cf154b742eb9bf4abc1
ju2hong <152269299+ju2hong@users.noreply.github.com>
2025-04-05 23:13:40 +0900

    sbb 질문등록 성공

--- springboot/Sbb/src/main/java/com/study/springboot/question/QuestionController.java
+++ springboot/Sbb/src/main/java/com/study/springboot/question/QuestionController.java
@@ -26,7 +26,7 @@ public class QuestionController {
         return "question_detail";
     }
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
     @PostMapping("/create")
