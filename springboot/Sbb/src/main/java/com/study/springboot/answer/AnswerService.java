f5475ce6026fafd8cc5e1ca7ecfa0d4f9b0e8c13
ju2hong <juihong730@gmail.com>
2025-04-07 13:00:33 +0900

    수정,삭제기능

--- springboot/Sbb/src/main/java/com/study/springboot/answer/AnswerService.java
+++ springboot/Sbb/src/main/java/com/study/springboot/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.study.springboot.answer;
 
+import com.study.springboot.DataNotFoundException;
 import com.study.springboot.question.Question;
 import com.study.springboot.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +21,21 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
