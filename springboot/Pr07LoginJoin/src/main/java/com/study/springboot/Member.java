2ebac29e971f69fa202bd7290f6d4c3c77f93945
ju2hong <juihong730@gmail.com>
2025-04-08 14:40:22 +0900

    로그인 회원가입 파일 추가

---
+++ springboot/Pr07LoginJoin/src/main/java/com/study/springboot/Member.java
@@ -0,0 +1,10 @@
+package com.study.springboot;
+
+import java.time.LocalDate;
+
+public class Member {
+    private String username;
+    private String password;
+    private String email;
+    private LocalDate joindate;
+}
