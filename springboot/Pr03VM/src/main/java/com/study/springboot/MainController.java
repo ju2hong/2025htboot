d8504080016d2517fdf977e71a03c2ea1e90fdee
ju2hong <juihong730@gmail.com>
2025-04-09 09:25:00 +0900

    자판기 코드 변경

---
+++ springboot/Pr03VM/src/main/java/com/study/springboot/MainController.java
@@ -0,0 +1,24 @@
+package com.study.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "redirect:/index";
+    }
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/add")
+    public String add() {
+        return "add";
+    }
+    @GetMapping("/edit")
+    public String edit() {
+        return "edit";
+    }
+}
