e70206f6fb3da58bd003be671ef98f949bec4e59
ju2hong <juihong730@gmail.com>
2025-04-11 15:44:41 +0900

    jpa, mysql로 crud 수업

--- springboot/Ex12H2DBTest/src/test/java/com/study/springboot/MemberRepositoryTest.java
+++ springboot/Ex12H2DBTest/src/test/java/com/study/springboot/MemberRepositoryTest.java
@@ -11,7 +11,8 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 //@SpringBootTest : 테스트환경을 기본설정해주는 어노테이션
 @SpringBootTest
@@ -20,15 +21,18 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @BeforeAll //static메소드, 모든 테스트 케이스 전에 호출
-    static void setup(){
+    static void setup() {
         System.out.println("@BeforeAll");
     }
-    @BeforeEach //@Test 메소드 호출시만다 한번 수행
-    void init(){
+
+    @BeforeEach
+        //@Test 메소드 호출시만다 한번 수행
+    void init() {
         System.out.println("@BeforeEach");
 
         save(); //레코드 추가
     }
+
     @Test //테스트 메소드(단위 테스트)
     @DisplayName("save 테스트")
     public void save() {
@@ -50,28 +54,31 @@ class MemberRepositoryTest {
         //assertEquals( 3, list.size() );
 
     }
+
     @Test
     @DisplayName("findById 테스트")
     public void findById() {
         // id가 1인 행을 검색(select)하여 행(레코드) 정보를 가져온다.
         Optional<MemberEntity> optional = memberRepository.findById(100L);
-        optional.ifPresent( unwrapped -> {
+        optional.ifPresent(unwrapped -> {
             //null이 아닐 때 호출되는 람다식
-            System.out.println("unwrapped. = " + unwrapped.getUserName() );
+            System.out.println("unwrapped. = " + unwrapped.getUserName());
             assertEquals("홍길동", unwrapped.getUserName());
-        } );
-        optional.ifPresentOrElse( (unwrapped) -> {
+        });
+        optional.ifPresentOrElse((unwrapped) -> {
             //null이 아닐 때 호출되는 람다식
-            System.out.println("unwrapped. = " + unwrapped.getUserName() );
+            System.out.println("unwrapped. = " + unwrapped.getUserName());
             assertEquals("홍길동", unwrapped.getUserName());
-        }, () -> fail("member 엔티티가 null입니다.") );
+        }, () -> fail("member 엔티티가 null입니다."));
     }
+
     @Test
     @DisplayName("count 테스트")
-    public void count(){
+    public void count() {
         Long count = memberRepository.count(); //select count(*) from member
         assertEquals(3, count);
     }
+
     @Test
     @DisplayName("쿼리 테스트")
     public void query() {
@@ -79,21 +86,134 @@ class MemberRepositoryTest {
 //        List<MemberEntity> list =
 //                memberRepository.findAll();
 //        assertEquals(3, list.size());
-//
+
 //        List<MemberEntity> list2 =
 //                //사용자 정의 쿼리 함수
 //                memberRepository.findByUserId("hong");
 //        assertEquals(1, list2.size());
+
         List<MemberEntity> list3 =
-                //사용자 정의 쿼리 함수
-                memberRepository.findByUserId("hong");
+                memberRepository.findFirst5ByUserIdAndUserNameOrderByIdDesc(
+                        "hong", "홍길동"
+                );
         assertEquals(1, list3.size());
+
+        Boolean isExist =
+                memberRepository.existsByJoindateLessThanEqual(LocalDate.now());
+        System.out.println("isExist = " + isExist);
+
+        long count = memberRepository.countByUserNameIgnoreCaseLike("%길%");
+        System.out.println("count = " + count);
     }
+
+    @Test
+    @DisplayName("update")
+    public void update() {
+        Optional<MemberEntity> optional = memberRepository.findById(1L);
+        optional.ifPresentOrElse(
+                (memberEntity) -> {
+                    memberEntity.update("hong2",
+                            memberEntity.getUserPw(),
+                            memberEntity.getUserName(),
+                            memberEntity.getUserRole());
+                    MemberEntity newMemberEntity = memberRepository.save( memberEntity ); //동일 ID값이 있으므로, Update됨.
+                    assertEquals("hong2", newMemberEntity.getUserId());
+                },
+                () -> {
+                    fail("memberEntity가 null입니다.");
+                });
+
+        Optional<MemberEntity> optional2 = memberRepository.findById(1L);
+        optional2.ifPresentOrElse(unwrapped -> {
+            assertEquals("hong2", unwrapped.getUserId());
+        }, () -> {
+            fail("1L 엔티티를 찾지 못했습니다.");
+        });
+    }
+    @Test
+    @DisplayName("delete")
+    public void delete() {
+        Optional<MemberEntity> optional = memberRepository.findById(10L);
+        optional.ifPresentOrElse( (entity)->{
+            memberRepository.delete(entity);
+
+            Optional<MemberEntity> optional2 = memberRepository.findById(1L);
+            assertEquals(false, optional2.isPresent());
+        }, ()->{
+            fail("entity가 null입니다.");
+        });
+    }
+
     @Test
     @DisplayName("JPQL 테스트")
     public void jpqlQuery() {
         List<MemberEntity> list =
                 memberRepository.findByUserId_JPQL("hong");
-        assertEquals(1,list.size());
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    @DisplayName("Native Query 테스트")
+    public void nativeQuery() {
+        List<MemberEntity> list =
+                memberRepository.findByUserId_Native("admin");
+        assertEquals(1, list.size());
+
+        int count = memberRepository.updateById_Native(1L, "gildong");
+        assertEquals(1, count);
+
+        Optional<MemberEntity> optional = memberRepository.findById(1L);
+        optional.ifPresentOrElse(unwrapped -> {
+            assertEquals("gildong", unwrapped.getUserId());
+        }, () -> {
+            fail("1L 엔티티를 찾지 못했습니다.");
+        });
+
+    }
+
+    @Test
+    @DisplayName("문제1")
+    public void ex01() {
+        boolean isExists = memberRepository.existsByUserPw("1234");
+        assertEquals(true, isExists);
+    }
+
+    @Test
+    @DisplayName("문제2")
+    public void ex02() {
+        LocalDate start = LocalDate.of(2025, 4, 1);
+        LocalDate end = LocalDate.of(2025, 4, 30);
+        boolean isExists = memberRepository.existsByJoindateBetween(start, end);
+        assertEquals(true, isExists);
+    }
+
+    @Test
+    @DisplayName("문제3")
+    public void ex03() {
+        boolean isExists = memberRepository.existsByUserId("hong");
+        assertEquals(true, isExists);
+
+    }
+
+    @Test
+    @DisplayName("문제3-2")
+    public void ex03_2() {
+        boolean isExists = memberRepository.existsByUserId("lee");
+        assertEquals(true, isExists);
+    }
+
+    @Test
+    @DisplayName("문제4")
+    public void ex04() {
+        int count = memberRepository.updateByUserId_Native(
+                "tom", "마이클", "3456");
+        assertEquals(1, count);
+
+        Optional<MemberEntity> optional = memberRepository.findByUserId("tom");
+        optional.ifPresentOrElse(unwrapped -> {
+            assertEquals("마이클", unwrapped.getUserName());
+        }, () -> {
+            fail("UserId(\"tom\") 엔티티를 찾지 못했습니다.");
+        });
     }
 }
---
+++ springboot/Ex14ReadCRUD/src/main/java/com/study/springboot/controller/HTMLController.java
@@ -0,0 +1,93 @@
+package com.study.springboot.controller;
+
+import com.study.springboot.dto.MemberSaveDto;
+import com.study.springboot.entity.MemberEntity;
+import com.study.springboot.entity.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class HTMLController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        List<MemberEntity> list = memberRepository.findAll();
+        model.addAttribute("list",list);
+        model.addAttribute("listcount",list.size());
+        return "index";
+    }
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+    @PostMapping("/joinAction")
+    @ResponseBody
+    public String joinAction(@ModelAttribute MemberSaveDto dto){
+
+        System.out.println("dto = " + dto.getId());
+        try {
+            MemberEntity entity = dto.toSaveEntity();
+            memberRepository.save(entity);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return "<script>alert('회원가입 실패 !'); history.back();</script>";
+        }
+        System.out.println("회원가입 성공");
+        return "<script>alert('회원가입 성공 !'); location.href='/';</script>";
+    }
+
+    //viewMember?id=1
+    @GetMapping("/viewMember")
+    public String viewMember(@RequestParam("id") Long id, Model model){
+        System.out.println("id = " + id);
+
+        Optional<MemberEntity> optional = memberRepository.findById(id);
+        if( !optional.isPresent() ) {
+            return "redirect:/list";
+        }
+
+        optional.ifPresent( (entity)->{
+            model.addAttribute("member", entity.toSaveDto());
+        });
+
+        return "modifyForm";
+    }
+        @PostMapping("/modifyAction")
+        @ResponseBody
+        public String modifyAction(@ModelAttribute MemberSaveDto dto) {
+            try{
+                MemberEntity entity = dto.toSaveEntity();
+                memberRepository.save(entity);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "<script>alert('회원정보 수정 실패'); history.back();</script>";
+            }
+            return "<script>alert('회원정보 수정 성공'); location.href='/';</script>";
+        }
+        @GetMapping("/deleteMember")
+        @ResponseBody
+        public String deleteMember(@RequestParam("id") Long id) {
+            Optional<MemberEntity> optional = memberRepository.findById(id);
+            if(!optional.isPresent()) {
+                return "<script>alert('회원정보 삭제 실패'); history.back();</script>";
+            }
+            MemberEntity entity = optional.get();
+            try {
+                memberRepository.delete(entity);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "<script>alert('회원정보 삭제 실패'); history.back();</script>";
+            }
+            return "<script>alert('회원정보 삭제 성공'); location.href='/';</script>";
+        } 
+    }
+
+
