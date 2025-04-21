18f5cd9cab9564757ef0d249df239bf528e3b590
ju2hong <juihong730@gmail.com>
2025-04-21 16:35:14 +0900

    게시판

---
+++ springboot/Ex24Board/src/main/java/com/study/springboot/controller/BoardController.java
@@ -0,0 +1,37 @@
+package com.study.springboot.controller;
+
+
+import com.study.springboot.dto.BoardResponseDto;
+import com.study.springboot.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/")
+    public String main(){
+        return "redirect:/board/listForm";
+    }
+    @GetMapping("/listForm")
+    public String listForm(Model model){
+        List<BoardResponseDto> list = boardService.findAll();
+        model.addAttribute("list", list);
+        return "listForm";
+    }
+
+
+}
+
+
+
+
+
