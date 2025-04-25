7ad56806ff06deff293ec461609267db1cbbe707
ju2hong <juihong730@gmail.com>
2025-04-25 09:39:17 +0900

    코드 수정

--- springboot/Ex26BoardApiServe/src/main/java/com/study/springboot/controller/BoardApiController.java
+++ springboot/Ex26BoardApiServe/src/main/java/com/study/springboot/controller/BoardApiController.java
@@ -6,6 +6,8 @@ import com.study.springboot.dto.ReplyResponseDto;
 import com.study.springboot.service.BoardService;
 import com.study.springboot.service.ReplyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +21,20 @@ public class BoardApiController {
 
     //게시글 전체 조회
     @GetMapping("/boards")
-    public List<BoardResponseDto> findAll() {
-        return boardService.findAll();
+    public ResponseEntity<List<BoardResponseDto>> findAll() {
+        List<BoardResponseDto> list = boardService.findAll();
+        return ResponseEntity.ok(list);
     }
     //게시글 단건 조회
     @GetMapping("/boards/{id}")
-    public BoardResponseDto findById(@PathVariable final Long id) {
-        return boardService.findById(id);
+    public ResponseEntity<BoardResponseDto> findById(@PathVariable final Long id) {
+        try {
+            BoardResponseDto dto = boardService.findById(id);
+            return ResponseEntity.ok(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
     }
     //게시글 댓글 조회
     @GetMapping("/boards/{id}/replys")
@@ -34,19 +43,31 @@ public class BoardApiController {
     }
     //게시글 추가
     @PostMapping("/boards")
-    public BoardResponseDto save(@RequestBody final BoardSaveRequestDto dto) {
-        return boardService.saveToDto(dto);
+    public ResponseEntity<BoardResponseDto> save(@RequestBody final BoardSaveRequestDto dto) {
+        BoardResponseDto savedDto = boardService.saveToDto(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedDto);
     }
     //게시글 수정
     @PutMapping("/boards/{id}")
-    public BoardResponseDto update(@PathVariable final Long id,
+    public ResponseEntity<BoardResponseDto> update(@PathVariable final Long id,
                                    @RequestBody final BoardSaveRequestDto dto) {
-        return boardService.updateToDto(id,dto);
+        try {
+            BoardResponseDto updateDto = boardService.updateToDto(id,dto);
+            return ResponseEntity.ok(updateDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
     }
     //게시글 삭제
     @DeleteMapping("/boards/{id}")
-    public Long delete(@PathVariable final Long id) {
-        return boardService.deleteToDto(id);
+    public ResponseEntity<Void> delete(@PathVariable final Long id) {
+        try {
+             boardService.delete(id);
+             return ResponseEntity.noContent().build(); //상태코드 204
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build(); //404
+        }
     }
 
 }
